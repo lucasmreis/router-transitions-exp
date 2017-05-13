7c3674f8d84001f913142e624db25b929dbd0546
Lucas Reis <lucasmreis@gmail.com>
2017-05-13 13:40:21 -0300

    routing transitions ok

--- src/App.js
+++ src/App.js
@@ -1,20 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { RouteTransition } from 'react-router-transition'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+
+const baseStyles = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  color: 'white',
+  width: '100vw',
+  height: '100vw'
+}
+
+const Home = () => (
+  <div style={{ ...baseStyles, backgroundColor: 'red'}}>
+    <h1>Home</h1>
+    <Link to="/about">
+      <button>About</button>
+    </Link>
+  </div>
+)
+
+const About = () => (
+  <div style={{ ...baseStyles, backgroundColor: 'green'}}>
+    <h1>About</h1>
+    <Link to="/">
+      <button>Home</button>
+    </Link>
+  </div>
+)
+
+const pushStyles = {
+  atEnter: { translateX: 100, opacity: 0 },
+  atLeave: { translateX: 0, opacity: 0 },
+  atActive: { translateX: 0, opacity: 1 }
+}
+
+const popStyles = {
+  atEnter: { translateX: -100, opacity: 0 },
+  atLeave: { translateX: 0, opacity: 0 },
+  atActive: { translateX: 0, opacity: 1 }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <Route render={({location, history, match}) => {
+          const routeStyles = history.action === 'POP' ? popStyles : pushStyles
+          console.log('---', history.action)
+          return (
+            <RouteTransition
+              pathname={location.pathname}
+              {...routeStyles}
+              runOnMount={false}
+              mapStyles={styles => ({
+                transform: `translateX(${styles.translateX}%)`,
+                opacity: styles.opacity || 1 })}>
+
+              <Switch key={location.key} location={location}>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About}/>
+              </Switch>
+
+            </RouteTransition>
+          )
+        }} />
+      </Router>
+    )
   }
 }
 
